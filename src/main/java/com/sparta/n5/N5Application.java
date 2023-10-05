9b84e7e1143c9cdc06d446a417f367220e519ba0
Kimhyeyun <cqqwer@gmail.com>
2023-10-05 20:48:45 +0900

    first commit

---
+++ src/main/java/com/sparta/n5/N5Application.java
@@ -0,0 +1,13 @@
+package com.sparta.n5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class N5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(N5Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sparta/n5/N5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.n5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class N5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
